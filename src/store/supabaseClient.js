71bd742434d940d87c04f3c601d569c455389324
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 00:13:36 +0300

    ..

--- src/store/supabaseClient.js
+++ src/store/supabaseClient.js
@@ -1,5 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-console.log(import.meta.env.MODE);
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+if (import.meta.env.DEV) {
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+} else if (import.meta.env.PROD) {
+  const supabaseUrl = process.env.VITE_SUPABASE_URL;
+  const supabaseKey = process.env.VITE_SUPABASE_KEY;
+}
+
 export const supabaseClient = createClient(supabaseUrl, supabaseKey);
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    svgr({
-      // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: {
-        ref: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+        "~": path.resolve(__dirname, "./"),
+        util: "util/",
+        zlib: "browserify-zlib",
       },
-    }),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      "~": path.resolve(__dirname, "./"),
-      util: "util/",
-      zlib: "browserify-zlib",
     },
-  },
-  define: {
-    "process.env": {},
-  },
+    define: {
+      "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+      "process.env.VITE_SUPABASE_KEY": JSON.stringify(env.VITE_SUPABASE_KEY),
+    },
+  };
 });
