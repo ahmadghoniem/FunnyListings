6274f162c194687cda55dd796e3d841ca405907c
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 08:58:30 +0300

    fix:vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import process from "process";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
       react(),
