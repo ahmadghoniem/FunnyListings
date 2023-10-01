d5704a9442739010440c1b25049dd7e67d15e67a
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:44:38 +0300

    u

--- src/store/supabaseClient.js
+++ src/store/supabaseClient.js
@@ -3,16 +3,18 @@ console.log(import.meta.env.MODE);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-console.log("exposed", import.meta.env.VITE_VERCEL_ENV);
 if (import.meta.env.VITE_VERCEL_ENV === "production" || import.meta.env.PROD) {
-  console.log("here");
   console.log("VITE_VERCEL_ENV", import.meta.env.VITE_VERCEL_ENV);
   console.log("PROD", import.meta.env.PROD);
 
   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
   const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-  const a7a = process.env.VITE_MAMA_MIA;
-  console.log(supabaseUrl, supabaseKey, a7a);
+  console.log(supabaseUrl, supabaseKey);
+
+  const a7a = VITE_MAMA_MIA;
+  const urrl = VITE_SUPABASE_URL;
+  const keyy = VITE_SUPABASE_KEY;
+  console.log(a7a, urrl, keyy);
   // } else if (import.meta.env.PROD) {
   //   const supabaseUrl = process.env.VITE_SUPABASE_URL;
   //   const supabaseKey = process.env.VITE_SUPABASE_KEY;
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,32 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [
-      react(),
-      svgr({
-        // svgr options: https://react-svgr.com/docs/options/
-        svgrOptions: {
-          ref: true,
-        },
-      }),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src/"),
-        "~": path.resolve(__dirname, "./"),
-        util: "util/",
-        zlib: "browserify-zlib",
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        ref: true,
       },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "~": path.resolve(__dirname, "./"),
+      util: "util/",
+      zlib: "browserify-zlib",
     },
-    define: {
-      // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
-      "process.env.VITE_MAMA_MIA": process.env.VITE_MAMA_MIA,
-      // "process.env": {},
-      // __APP_ENV__: process.env.VITE_VERCEL_ENV,
-      VITE_SUPABASE_URL: JSON.stringify(env.VITE_SUPABASE_URL),
-      VITE_SUPABASE_KEY: JSON.stringify(env.VITE_SUPABASE_KEY),
-    },
-  };
+  },
+  define: {
+    // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+    VITE_MAMA_MIA: JSON.stringify(process.env.VITE_MAMA_MIA),
+    VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL),
+    VITE_SUPABASE_KEY: JSON.stringify(process.env.VITE_SUPABASE_KEY),
+    // "process.env": {},
+    // __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
 });
