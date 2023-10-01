3f6da2a7ace48e35238c3cf1beb44f331397d688
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:25:30 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import process from "process";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
