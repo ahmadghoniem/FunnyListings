f16d6a232c8fa16f51408c852fe8a95ef146bd16
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:04:48 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,9 @@ export default defineConfig(({ command, mode }) => {
     define: {
       // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
       // "process.env.VITE_SUPABASE_KEY": JSON.stringify(env.VITE_SUPABASE_KEY),
-      "process.env": {},
+      "process.env": process.env,
+      VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+      VITE_SUPABASE_KEY: process.env.VITE_SUPABASE_KEY,
     },
   };
 });
