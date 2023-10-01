6797dbf8d7adea1c6814ae1aeb6b7d9063029469
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 13:51:09 +0300

    tst

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: {
-        ref: true,
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react(),
+      svgr({
+        // svgr options: https://react-svgr.com/docs/options/
+        svgrOptions: {
+          ref: true,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@":
+          mode === "development"
+            ? require("path").resolve(__dirname, "./src")
+            : "",
+        "~":
+          mode === "development"
+            ? require("path").resolve(__dirname, "./")
+            : "",
+        util: "util/",
+        zlib: "browserify-zlib",
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      "~": path.resolve(__dirname, "./"),
-      util: "util/",
-      zlib: "browserify-zlib",
     },
-  },
-  define: {
-    // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
-    // VITE_MAMA_MIA: JSON.stringify(process.env.VITE_MAMA_MIA),
-    // VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL),
-    // VITE_SUPABASE_KEY: JSON.stringify(process.env.VITE_SUPABASE_KEY),
-    // __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
+    define: {
+      // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+      // VITE_MAMA_MIA: JSON.stringify(process.env.VITE_MAMA_MIA),
+      // VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL),
+      // VITE_SUPABASE_KEY: JSON.stringify(process.env.VITE_SUPABASE_KEY),
+      // __APP_ENV__: process.env.VITE_VERCEL_ENV,
+      "process.env": {},
+    },
+  };
 });
+
+// "~": import("path").then((path) => path.resolve(__dirname, "./")),
