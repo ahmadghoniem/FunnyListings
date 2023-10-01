c1987af4dbb2ece7940722d574855e8521b29424
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:33:20 +0300

    u

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
-import process from "process";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -27,10 +26,16 @@ export default defineConfig(({ command, mode }) => {
     define: {
       // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
       // "process.env.VITE_SUPABASE_KEY": JSON.stringify(env.VITE_SUPABASE_KEY),
-      "process.env": {},
+      // "process.env": {},
       // __APP_ENV__: process.env.VITE_VERCEL_ENV,
       VITE_SUPABASE_URL: JSON.stringify(env.VITE_SUPABASE_URL),
       VITE_SUPABASE_KEY: JSON.stringify(env.VITE_SUPABASE_KEY),
+      "process.env.VITE_SUPABASE_URL": JSON.stringify(
+        process.env.VITE_SUPABASE_URL,
+      ),
+      "process.env.VITE_SUPABASE_KEY": JSON.stringify(
+        process.env.VITE_SUPABASE_KEY,
+      ),
     },
   };
 });
