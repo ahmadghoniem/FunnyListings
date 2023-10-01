f36225636f6a734b9d40aa4545654fbcd36276e7
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:15:47 +0300

    TEST

--- src/store/supabaseClient.js
+++ src/store/supabaseClient.js
@@ -3,11 +3,14 @@ console.log(import.meta.env.MODE);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-console.log("exposed", VITE_VERCEL_ENV);
-if (VITE_VERCEL_ENV === "production") {
-  //   console.log("here");
-  const supabaseUrl = VITE_SUPABASE_URL;
-  const supabaseKey = VITE_SUPABASE_KEY;
+console.log("exposed", import.meta.env.VITE_VERCEL_ENV);
+if (import.meta.env.VITE_VERCEL_ENV === "production" || import.meta.env.PROD) {
+  console.log("here");
+  console.log("VITE_VERCEL_ENV", import.meta.env.VITE_VERCEL_ENV);
+  console.log("PROD", import.meta.env.PROD);
+
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
   // } else if (import.meta.env.PROD) {
   //   const supabaseUrl = process.env.VITE_SUPABASE_URL;
   //   const supabaseKey = process.env.VITE_SUPABASE_KEY;
