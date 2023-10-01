1d173e11de3af14970c8f0339f4a1489bd81fb75
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-01 09:53:29 +0300

    ..

--- src/store/supabaseClient.js
+++ src/store/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-console.log(import.meta.env.MODE);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
@@ -7,13 +6,15 @@ if (import.meta.env.VITE_VERCEL_ENV === "production" || import.meta.env.PROD) {
   console.log("VITE_VERCEL_ENV", import.meta.env.VITE_VERCEL_ENV);
   console.log("PROD", import.meta.env.PROD);
 
+  console.log("using import.meta.");
   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
   const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
   console.log(supabaseUrl, supabaseKey);
 
-  const a7a = VITE_MAMA_MIA;
-  const urrl = VITE_SUPABASE_URL;
-  const keyy = VITE_SUPABASE_KEY;
+  console.log("usingprocess.env.");
+  const a7a = process.env.VITE_MAMA_MIA;
+  const urrl = process.env.VITE_SUPABASE_URL;
+  const keyy = import.meta.env.VITE_SUPABASE_KEY;
   console.log(a7a, urrl, keyy);
   // } else if (import.meta.env.PROD) {
   //   const supabaseUrl = process.env.VITE_SUPABASE_URL;
