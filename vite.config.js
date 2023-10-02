66ede4f28a13d30780bd005fa7da0cca5e0a6359
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-02 13:35:03 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [
@@ -35,15 +35,15 @@ export default defineConfig(({ command, mode }) => {
         "~": path.resolve(__dirname, "./"),
       },
     },
-    // define: {
-    //   // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
-    //   __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    //   // __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    //   SHEKO: JSON.stringify(process.env.VITE_SUPABASE_URL),
+    define: {
+      // "process.env.VITE_SUPABASE_URL": JSON.stringify(env.VITE_SUPABASE_URL),
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+      // __APP_ENV__: process.env.VITE_VERCEL_ENV,
+      SHEKO: JSON.stringify(process.env.VITE_SUPABASE_URL),
 
-    //   BEKO: JSON.stringify(env.VITE_MAMA_MIA),
-    //   SEKO: JSON.stringify(process.env.VITE_MAMA_MIA),
-    // },
+      BEKO: JSON.stringify(env.VITE_MAMA_MIA),
+      SEKO: JSON.stringify(process.env.VITE_MAMA_MIA),
+    },
   };
 });
 
