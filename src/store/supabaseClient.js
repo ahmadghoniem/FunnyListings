840500cd55055ba8ac18171ba964c4045147fc8e
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-02 13:55:46 +0300

    test

--- src/store/supabaseClient.js
+++ src/store/supabaseClient.js
@@ -9,15 +9,9 @@ if (import.meta.env.VITE_VERCEL_ENV === "production" || import.meta.env.PROD) {
   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
   const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-  console.log(
-    "VITE_MAMA_MIA using import.meta.en ",
-    import.meta.env.VITE_MAMA_MIA,
-  );
-  console.log("VITE_MAMA_MIA using JSON.stringify(env.VITE_MAMA_MIA) ", BEKO);
-  console.log(
-    "VITE_MAMA_MIA JSON.stringify(process.env.VITE_MAMA_MIA), ",
-    SEKO,
-  );
+  console.log("MAMA_MIA using import.meta.en ", import.meta.env.MAMA_MIA);
+  console.log("MAMA_MIA using JSON.stringify(env.MAMA_MIA) ", BEKO);
+  console.log("MAMA_MIA JSON.stringify(process.env.MAMA_MIA), ", SEKO);
   console.log("using process.env.VITE_VERCEL_ENV,", __APP_ENV__);
 }
 export const supabaseClient = createClient(supabaseUrl, supabaseKey);
