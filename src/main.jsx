535dc97f67bee98344c6bc605d9ce5285066e908
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-02 13:43:14 +0300

    فثسف

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,5 @@ console.log(
 );
 console.log("VITE_MAMA_MIA using JSON.stringify(env.VITE_MAMA_MIA) ", BEKO);
 console.log("VITE_MAMA_MIA JSON.stringify(process.env.VITE_MAMA_MIA), ", SEKO);
-console.log("using process.env.VITE_VERCEL_ENV,", __APP_ENV__);
+// console.log("using process.env.VITE_VERCEL_ENV,", __APP_ENV__);
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
